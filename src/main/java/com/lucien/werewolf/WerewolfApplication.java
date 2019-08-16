f000fae69ea78f449ef0d06905658f3fd29d1bbb
syYing <706580680@qq.com>
2019-08-16 11:28:38 +0800

    Initial commit

---
+++ src/main/java/com/lucien/werewolf/WerewolfApplication.java
@@ -0,0 +1,13 @@
+package com.lucien.werewolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerewolfApplication.class, args);
+    }
+
+}
